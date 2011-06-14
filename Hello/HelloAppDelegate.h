c31f1130a4237a62540bb1af5ec6d0931024853b
Janos Kukoda <kukodajanos@freemail.hu>
2011-06-14 03:25:49 +0200

    Initial Commit

---
+++ Hello/HelloAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloAppDelegate.h
+//  Hello
+//
+//  Created by János Kukoda on 6/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
